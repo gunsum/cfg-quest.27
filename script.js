0b56eb0c9a6268b94e2472673d5a1a3533ebc327
Fatma Oymak <bedizma@hotmail.com>
2023-01-14 00:07:50 +0000

    answer is

---
+++ script.js
@@ -0,0 +1,12 @@
+filmOne = "Ferris Bueller\'s Day Off"
+filmTwo = "Ferris Bueller\'s Day Off"
+
+if (filmOne == filmTwo) {
+    console.log("I love the Ferris Bueller movie!")
+}
+
+if (filmOne = filmTwo) {
+    console.log("I love the Ferris Bueller movie!")
+}
+
+console.log("Program is finished executing!")
